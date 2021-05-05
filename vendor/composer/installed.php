2ec3e46270f11d4afc996fb47a713a8f8a0d4822
Mahadi997 <79517252+Mahadi997@users.noreply.github.com>
2021-05-05 10:10:43 +0200

    Installing Flight PHP

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '72f77dfa53e494571c248196a8a3f7ed17a31930',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '72f77dfa53e494571c248196a8a3f7ed17a31930',
+    ),
+    'mikecao/flight' => 
+    array (
+      'pretty_version' => 'v1.3.9',
+      'version' => '1.3.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bc5e03ca59accb3ef37e31b6d4a2a9c78c7bb7ce',
+    ),
+  ),
+);
